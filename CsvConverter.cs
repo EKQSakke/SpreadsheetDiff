e73e6fe7c372cc4df1c43110fdb312e1684c0a56
Sakari Ekqvist <sakari.ekqvist@magicad.com>
2024-01-21 13:52:52 +0200

    Basic conversion working

---
+++ CsvConverter.cs
@@ -0,0 +1,65 @@
+﻿static class CsvConverter
+{
+    public static void Convert(string input, string output)
+    {
+        var wb = Workbook.Load(input);
+        foreach (var sheet in wb.Worksheets)
+        {
+            Console.WriteLine($"=== {sheet.SheetName} ===");
+            Console.WriteLine($"{sheet.Cells.Count}, {sheet.Columns.Count}");
+            var rows = new List<string>();
+            var columnCount = GetColumnCount(sheet);
+
+            Console.WriteLine($"Column count {columnCount}");
+
+            var columnModulo = 0;
+            var sb = new StringBuilder();
+            foreach (var cell in sheet.Cells.Keys)
+            {
+                if (columnModulo % 3 == 0)
+                {
+                    if (sb.Length != 0)
+                    {
+                        rows.Add(sb.ToString());
+                    }
+
+                    sb.Clear();
+                }
+                else
+                {
+                    sb.Append(";");
+                }
+
+                sb.Append(sheet.Cells[cell].Value);
+                columnModulo++;
+            }
+
+            rows.Add(sb.ToString());
+
+            var file = File.Create(output);
+            using var sw = new StreamWriter(file);
+            foreach (var row in rows)
+            {
+                Console.WriteLine($"{row}");
+                sw.WriteLine($"{row}");
+            }
+        }
+    }
+
+    static int GetColumnCount(Worksheet sheet)
+    {
+        var columnCount = 0;
+        foreach (var cell in sheet.Cells.Keys)
+        {
+            if (cell.Last() == '1')
+            {
+                columnCount++;
+                continue;
+            }
+
+            break;
+        }
+
+        return columnCount;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿var input = "C:/temp/Base.xlsx";
+var output = "C:/temp/Output.csv";
+
+Console.WriteLine("Args:");
+
+if (args.Length == 0)
+{
+    Console.WriteLine("No arguments given");
+}
+else if (args.Length == 1 && args[1] == "c")
+{
+    Console.WriteLine("Converting with placeholder files");
+    CsvConverter.Convert(input, output);
+}
+else if (args.Length == 3 && args[1] == "c")
+{
+    Console.WriteLine($"Converting {args[2]} to csv: {args[3]}");
+    CsvConverter.Convert(args[2], args[3]);
+}
